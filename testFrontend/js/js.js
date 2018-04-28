b509594e9b088ad25ba7a925f7adb1740cf32c63
nivel1.desar <nivel1.desar@DESKTOP-LHAHP3M.groupcos.com>
2018-04-28 17:21:04 -0500

    Se crean las clases para la consulta de las ciudades para la reserva de tiquetes y la consulta de vuelos

--- testFrontend/js/js.js
+++ testFrontend/js/js.js
@@ -1,5 +1,57 @@
-
+//////FUNCIONES QUE SE EJECUTAN TAN PRONTO EL DOCUMENTO TERMINA DE CARGAR DE FORMA COMPLETA
 $(document).ready(function () {
+
+
+/////////////////AJAX QUE SE EJECUTA PARA CONSULTAS LAS CIUDADES
+    var ciudadOrigen = $("#ciudadOrigen");
+
+    $.ajax({
+        type: "GET",
+        url: "../backEnd/procesos.php?consulta_ciudad",
+        success: function (resulta)
+        {
+            var result = jQuery.parseJSON(resulta);
+
+            var output = "<option>Ciudad de Origen</option>";
+
+            //////UTILIZO EL FOR PARA RECORRER E IMPRIMIR CADA UNA DE LAS CIUDADES DE LA BD
+            for (var i in result) {
+                output += "<option value='" + result[i].id + "'>" + result[i].nombre + "</option>";
+            }
+
+            output += "";
+
+            ///////////IMPRIME LO QUE TRAE EN EL SELECT ESPECIAFICADO  
+            ciudadOrigen.html(output);
+
+        }});
+
+
+    /////////////////AJAX QUE SE EJECUTA PARA CONSULTAS LAS CIUDADES PARA EL FILTRO
+
+    var ciudadOrigenConsulta = $("#ciudadOrigenConsulta");
+
+    $.ajax({
+        type: "GET",
+        url: "../backEnd/procesos.php?ciudadConsulta",
+        success: function (resulta)
+        {
+            var result = jQuery.parseJSON(resulta);
+
+            var output = "";
+
+            //////UTILIZO EL FOR PARA RECORRER E IMPRIMIR CADA UNA DE LAS CIUDADES DE LA BD
+
+            for (var i in result) {
+                output += "<option value='" + result[i].id + "'>" + result[i].nombre + "</option>";
+            }
+
+            output += "";
+
+///////////IMPRIME LO QUE TRAE EN EL SELECT ESPECIAFICADO  
+            ciudadOrigenConsulta.html(output);
+        }});
+
 
     function ocultar(clase) {
         $("." + clase).hide('fast').find('input,select').removeAttr('required').val('');
@@ -13,31 +65,110 @@ $(document).ready(function () {
     ocultar('camposPersonales');
     ocultar('divDestino');
 
-    $("#ciudadOrigen").change(function () {
-        $("#cargando").html("<center><img src='img/spinner1.gif' width='120px'></center>");
-        mostrar('divDestino');
-    });
 
+    ////////////////FUNCION QUE SE EJECUTA CUANDO EL SELECT CIUDADORIGEN CAMBIA DE OPTION
+
+    $("#ciudadOrigen").change(function () {
+        ////////////////GUARDO EL VALOR SELECCIONADO EN UNA VARIABLE PARA REALIZAR UNA NUEVA CONSULTA SIN LA CIUDAD QUE YA SE ENCOGIO ANTERIORMENTE
+        var ciudadOrigen = $(this).val();
+
+        var ciudadDestino = $("#ciudadDestino");
+
+        $("#cargando").html("<center><img src='img/spinner1.gif' width='120px'></center>");
+
+        $.ajax({
+            type: "GET",
+            url: "../backEnd/procesos.php?consulta_ciudad&ciudadOrigen=" + ciudadOrigen,
+            success: function (resulta)
+            {
+                var result = jQuery.parseJSON(resulta);
+
+                var output = "";
+                for (var i in result) {
+                    output += "<option value='" + result[i].id + "'>" + result[i].nombre + "</option>";
+                }
+                console.log(result);
+
+                output += "";
+
+                ciudadDestino.html(output);
+
+            }});
+
+        mostrar('divDestino');
+
+    });
+    
+    
     $("#ciudadDestino").change(function () {
         $("#cargando").html("");
         mostrar('camposPersonales');
+
+    });
+    
+
+    ////////////////FUNCION QUE SE EJECUTA CUANDO EL SELECT CIUDADORIGEN PARA FILTRO
+
+    $("#ciudadOrigenConsulta").change(function () {
+
+        ////////////////GUARDO EL VALOR SELECCIONADO EN UNA VARIABLE PARA REALIZAR UNA NUEVA CONSULTA SIN LA CIUDAD QUE YA SE ENCOGIO ANTERIORMENTE
+        var ciudadOrigenConsulta = $(this).val();
+
+        console.log(ciudadOrigenConsulta);
+
+        var ciudadOrigenConsultaDestino = $("#ciudadOrigenConsultaDestino");
+
+        $.ajax({
+            type: "GET",
+            url: "../backEnd/procesos.php?ciudadConsulta&ciudadOrigenConsulta=" + ciudadOrigenConsulta,
+            success: function (resulta)
+            {
+                var result = jQuery.parseJSON(resulta);
+
+                var output = "";
+                for (var i in result) {
+                    output += "<option value='" + result[i].id + "'>" + result[i].nombre + "</option>";
+                }
+                console.log(result);
+
+                output += "";
+                ciudadOrigenConsultaDestino.html(output);
+
+            }});
+
     });
 
-    $("#agregarTarea").submit(function (e) {
-        $("#cargando").html("<center><img src='img/spinner1.gif' width='120px'></center>");
-        var url = "ajax/insercion.php";
+    $("#consultaVuelo").submit(function (e) {
+
+        $("#cargandoVuelos").html("<center><img src='img/spinner1.gif' width='120px'></center>");
+
+        var url = "../backEnd/procesos.php";
         $.ajax({
             type: "POST",
             url: url,
-            data: $("#agregarTarea").serialize(),
-            success: function (registro) {
+            data: $("#consultaVuelo").serialize(),
+            success: function (resulta) {
 
-                if (registro == 1) {
-                    $("#cargando").html("<center><div style='margin-top:10px;' class='alert alert-info'>Se ha registrado la tarea con éxito.</div></center>");
-                    $("#agregarTarea")[0].reset();
-                } else {
-                    alert("No se ha podido ingresar el registro");
+                var result = jQuery.parseJSON(resulta);
+                
+                $("#tabla").html("");
+                
+                var output = "";
+                
+                for (var i in result) {
+          
+                     output +="<tr>"
+                        + "<td>" + result[i].ciudadOrigen + "</td>"
+                        + "<td>" + result[i].ciudadDestino + "</td>"
+                        + "<td>" + result[i].fecha_salida + "</td>"
+                        + "<td>" + result[i].duracion + "</td>"
+                    + "</tr>";
+                
                 }
+                
+                output += "";
+                $("#cargandoVuelos").html("");
+                $("#agragarCampos").html(output);
             }
         });
         e.preventDefault();
